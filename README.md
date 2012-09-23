cd6ad31ea8e65ec82b0f91352450a2b1217c460d
Karim <madow2@yahoo.com>
2012-09-23 17:52:04 -0400

    updating the initial commit with all the needed modules and updating couple files for the initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Famlist
+
+### Description
+Open Source Family Shopping List
+
+### Recommendations
+* Use Linux or OS X (check known issues section)
+
+### Installation
+1. Install node.js 0.6.0 or greater
+2. Install mongodb 2.0.7
+3. execute npm install from the source root directory
+
+### Known Issues
+1. mongodb module will not install on Windows due to the inability to compile bson. This is an issue with the bson module and not npm or this application.
--- server.js
+++ server.js
@@ -11,13 +11,11 @@ zcache['index.html'] = fs.readFileSync('./views/index.html'); //  Cache index.ht
 // Create "express" server.
 var app  = express.createServer();
 
- app.configure(function(){
-   app.use(express.bodyParser());
-   app.use(express.static(__dirname + '/public'));
-   //app.set('view engine', 'jade');
- });
-
-
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
+    app.set('view engine', 'ejs');
+});
 
 /*  =====================================================================  */
 /*  Setup route handlers.  */
@@ -33,49 +31,49 @@ app.get('/asciimo', function(req, res){
     var link="https://a248.e.akamai.net/assets.github.com/img/d84f00f173afcf3bc81b4fad855e39838b23d8ff/687474703a2f2f696d6775722e636f6d2f6b6d626a422e706e67";
     res.send("<html><body><img src='" + link + "'><h1>testing html</h1></body></html>");
 });
- 
+
 // Handler for GET /
 app.get('/', function(req, res){
     res.send(zcache['index.html'], {'Content-Type': 'text/html'});
 });
 
-
 //  Get the environment variables we need.
 var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP;
 var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+//var ipaddr  = '127.0.0.1';
+//var port    = process.env.PORT || 8080;
 
 if (typeof ipaddr === "undefined") {
-   console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
+    console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
 }
 
 //  terminator === the termination handler.
 function terminator(sig) {
-   if (typeof sig === "string") {
-      console.log('%s: Received %s - terminating Node server ...',
-                  Date(Date.now()), sig);
-      process.exit(1);
-   }
-   console.log('%s: Node server stopped.', Date(Date.now()) );
+    if (typeof sig === "string") {
+        console.log('%s: Received %s - terminating Node server ...',
+            Date(Date.now()), sig);
+        process.exit(1);
+    }
+    console.log('%s: Node server stopped.', Date(Date.now()) );
 }
 
 //  Process on exit and signals.
 process.on('exit', function() { terminator(); });
 
 ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
- 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'
+    'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'
 ].forEach(function(element, index, array) {
-    process.on(element, function() { terminator(element); });
-});
+        process.on(element, function() { terminator(element); });
+    });
 
 //  And start the app on that interface (and port).
 app.listen(port, ipaddr, function() {
-   console.log('%s: Node server started on %s:%d ...', Date(Date.now() ),
-               ipaddr, port);
+    console.log('%s: Node server started on %s:%d ...', Date(Date.now() ),
+        ipaddr, port);
 });
 
 //log errors to cosole
 app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.send(500, 'Something broke!');
+    console.error(err.stack);
+    res.send(500, 'Something broke!');
 });
-
