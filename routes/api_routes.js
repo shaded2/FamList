372baa0abb1e9bd9f031296f060fb6071ce1560e
Karim <madow2@yahoo.com>
2012-10-04 01:04:29 -0400

    Retrieve data from the database and display on the home page.

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,15 +3,10 @@ module.exports = function(app, models, mongoose){
     /*  Setup api route handlers.  */
     /*  =====================================================================  */
 
-    // Handler for GET /api/health
-    app.get('/api/health', function(req, res){
-        res.send('1');
-    });
-
     // Handler for GET /api/examples
     app.get('/api/shopping_items', function(req, res){
         //get all the shopping items
-        models.shopping_items.find({}, function(err, docs){
+        models.shopping_items.find({}, '_id description owner', function(err, docs){
             if (!err) {
                 return res.send(docs);
             }
@@ -19,5 +14,10 @@ module.exports = function(app, models, mongoose){
                 return console.log(err);
             }
         });
+    });
+
+    // Handler for GET /api/health
+    app.get('/api/health', function(req, res){
+        res.send('1');
     });
 };
--- routes/app_routes.js
+++ routes/app_routes.js
@@ -3,22 +3,22 @@ module.exports = function(app, models, mongoose){
     /*  Setup application route handlers.  */
     /*  =====================================================================  */
 
-    // Handler for GET /health
-    app.get('/health', function(req, res){
-        res.send('1');
-    });
-
     // Handler for GET /
     app.get('/', function(req, res){
-        //get all the shopping items
-        models.shopping_items.find({}, function(err, docs){
+        //get all the shopping items, only need the id, description and ownner
+        models.shopping_items.find({}, '_id description owner', function(err, docs){
             res.render( 'index.ejs', {
                 locals: {
-                    title: 'FamList - A shopping list for the entire family, Node.js',
+                    title: 'FamList - A shopping list for the entire family',
                     page: 'index',
                     items: docs
                 }
             });
         });
+    });
+
+    // Handler for GET /health
+    app.get('/health', function(req, res){
+        res.send('1');
     });
 };
