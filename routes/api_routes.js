5e4c457a43ebc25f1895c5fbb579a71f08b64aa4
Karim <madow2@yahoo.com>
2012-10-04 01:15:51 -0400

    adding versions to the api

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,9 +3,14 @@ module.exports = function(app, models, mongoose){
     /*  Setup api route handlers.  */
     /*  =====================================================================  */
 
+    // Handler for GET /api/health
+    app.get('/api/health', function(req, res){
+        res.send('1');
+    });
+
     // Handler for GET /api/examples
-    app.get('/api/shopping_items', function(req, res){
-        //get all the shopping items
+    app.get('/api/v1/shopping_items', function(req, res){
+        //get all the shopping items, only need the id, description and ownner
         models.shopping_items.find({}, '_id description owner', function(err, docs){
             if (!err) {
                 return res.send(docs);
@@ -14,10 +19,5 @@ module.exports = function(app, models, mongoose){
                 return console.log(err);
             }
         });
-    });
-
-    // Handler for GET /api/health
-    app.get('/api/health', function(req, res){
-        res.send('1');
     });
 };
