6ec0f062527463d9473b7349382ba8a907bd5d94
Karim <madow2@yahoo.com>
2012-10-04 00:00:23 -0400

    Removing the useless route

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -8,12 +8,6 @@ module.exports = function(app, models, mongoose){
         res.send('1');
     });
 
-    // Handler for GET /asciimo
-    app.get('/asciimo', function(req, res){
-        var link="https://a248.e.akamai.net/assets.github.com/img/d84f00f173afcf3bc81b4fad855e39838b23d8ff/687474703a2f2f696d6775722e636f6d2f6b6d626a422e706e67";
-        res.send("<html><body><img src='" + link + "'><h1>testing html</h1></body></html>");
-    });
-
     // Handler for GET /
     app.get('/', function(req, res){
         //get all the shopping items
